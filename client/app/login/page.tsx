cfde1b5fcc86208124ba028c52b1ec238118f0df
João <joao.lutti11@gmail.com>
2024-03-04 11:40:47 -0700

    more login page

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -16,7 +16,7 @@ export default function Login(){
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                 <img
-                    className="mx-auto max-h-26"
+                    className="mx-auto max-h-[100px]"
                     src="/learnlink-logo.webp"
                     alt="Learnlink"
                 />
